790aa5606b4438ed8faf997809cf057b3a77e82d
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-09 13:22:50 +0530

    [Afrath] Updated the file with Search data from Linked List

--- UnitTestLinkedList/UnitTest1.cs
+++ UnitTestLinkedList/UnitTest1.cs
@@ -1,10 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinkedList;
 
 namespace UnitTestLinkedList
 {
+    
     [TestClass]
     public class UnitTest1
     {
+        [TestInitialize]
+        public void SetUp()
+        {
+            
+        }
+        
         [TestMethod]
         public void TestMethod1()
         {
