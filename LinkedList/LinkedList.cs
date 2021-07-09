a9b146fccc6aaca8ea8d84a57cb408d608352a52
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-09 11:19:46 +0530

    [Afrath] Updated the file with Add Last Use Case

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        public Node head;
+        public void InsertAtLast(int data)
+        {
+            Node newnode = new Node(data);
+            if(head == null)
+            {
+                this.head = newnode;
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = newnode;
+            }
+
+        }
+        public Node GetLastNode()
+        {
+            Node temp = this.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        public void Display()
+        {
+          
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.WriteLine("Element of Linked List: {0} ", temp.data);
+                temp = temp.next;
+               
+            }
+        }
+    }
+}
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,6 +7,13 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Program!");
+            LinkedList linkedList = new LinkedList();
+            linkedList.InsertAtLast(56);
+            linkedList.InsertAtLast(30);
+            linkedList.InsertAtLast(70);
+            Console.WriteLine("Elements are inserted at last");
+            linkedList.Display();
+
         }
     }
 }
