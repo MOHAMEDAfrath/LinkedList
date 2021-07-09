927e133795fcdfadba838e08098dee1ee0a8daa1
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-09 11:33:55 +0530

    [Afrath] Updated the file with Append to Linked List

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -9,6 +9,8 @@ namespace LinkedList
             Console.WriteLine("Welcome to Linked List Program!");
             LinkedList linkedList = new LinkedList();
             Console.WriteLine("Enter 1 to Add at Last");
+            Console.WriteLine("Enter 2 to Add at First");
+            Console.WriteLine("Enter 3 to Append date to Linked List");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -26,7 +28,13 @@ namespace LinkedList
                     Console.WriteLine("Elements are inserted at first");
                     linkedList.Display();
                     break;
-
+                case 3:
+                    linkedList.InsertAtLast(56);
+                    linkedList.InsertAtLast(30);
+                    linkedList.InsertAtLast(70);
+                    Console.WriteLine("Elements are appended at last");
+                    linkedList.Display();
+                    break;
 
             }
            
